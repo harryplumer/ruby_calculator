bc32a224ee3a3c49c03c7dc1f595f83fa2ee2811
Harry Plumer <harry.plumer@gmail.com>
2017-08-16 15:25:49 -0600

    all basic functions completed

--- calculator.rb
+++ calculator.rb
@@ -2,11 +2,11 @@
 require 'pry'
 
 def get_num
-  num = Integer(gets.strip) rescue false
+  num = Float(gets.strip) rescue false
   if (num)
-    num = num.to_i
+    num = num.to_f
   else
-    puts "Please Enter A Valid Integer"  
+    puts "Please Enter A Valid Number"  
     get_num()
   end
 end
@@ -15,6 +15,8 @@ def get_operator
   operator = gets.strip
     if operator == "+" || operator == "-" || operator == "*" || operator == "/"
       operator
+    elsif operator == "C" || operator == "CLEAR" || operator == "clear" || operator == "c"
+      operator = "C"
     else
       puts "\"#{operator}\" is not a valid operator. Only the \"+, -, *, /\" operators are supported. Please enter a valid operator below"
       get_operator
@@ -35,15 +37,28 @@ def get_answer(first_num, operation, second_num)
 end
 
 puts "Welcome to the Ruby Calculator"
-puts "What is the first number?"
-first_num = get_num
-puts "What is the operator?"
-operation = get_operator
-puts "What is the second number?"
-second_num = get_num
-puts "Calculating..."
-puts "The result of #{first_num} #{operation} #{second_num} is #{get_answer(first_num, operation, second_num)}"
+first_num = nil
 
+while true
+  if (!first_num)
+    puts "What is the first number?"
+    first_num = get_num
+  end
+  puts "Enter the operator or enter 'C' to clear"
+  operation = get_operator
+  if (operation == "C")
+    first_num = nil
+    puts "Clearing..."
+    next
+  else
+    puts "What is the second number?"
+    second_num = get_num
+    puts "Calculating..."
+    answer = get_answer(first_num, operation, second_num)
+    puts "The result of #{first_num.round(2)} #{operation} #{second_num.round(2)} is #{answer.round(2)}"
+    first_num = answer
+  end
+end
 
 
 
