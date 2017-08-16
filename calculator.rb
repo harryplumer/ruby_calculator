d3f98698bfa21edee640e5309501aede00f813d7
Harry Plumer <harry.plumer@gmail.com>
2017-08-16 14:27:16 -0600

    working getNum function

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,21 @@
 #all requires go on top
 require 'pry'
 
-puts "Welcome to the ruby calculator"
+def getNum()
+  num = Integer(gets.strip) rescue false
+  if (num)
+    num = num.to_i
+  else
+    puts "Please Enter A Valid Integer"  
+    getNum()
+  end
+end
 
-#TODO: your code goes here!
+puts "Welcome to the Ruby Calculator"
+puts "What is the first number?"
+first_num = getNum()
+puts first_num
+
+
+
 
